84a5953a64fa362d4739f6e7a0214e67ea78b13c
prabhnith <prabhjot.nith@gmail.com>
2019-07-03 12:33:49 +0530

    Dashboard: Eventcard added, server fetching all events list

--- client/src/Components/Dashboard/AllEvents.js
+++ client/src/Components/Dashboard/AllEvents.js
@@ -1,18 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import EventCard from "../EventCard";
+import Grid from "@material-ui/core/Grid";
+// import Paper from "@material-ui/core/Paper";
 
 export default class AllEvents extends Component {
-    constructor(){
-        super();
-        this.state={
-            Events:[]
-        }
-    }
-    render(){
-        return(
-            <div>
-                <h1>All Events come here</h1>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      Events: []
+    };
+  }
+  componentDidMount() {
+    fetch("http://localhost:3001/getAllEvents", {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(json => json.data)
+      .then(eventsArray => {
+        console.log(eventsArray);
+        this.setState({ Events: eventsArray });
+      });
+  }
+  render() {
+    return (
+      <Grid
+        container
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+        spacing={3}
+        item
+      >
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+        <Grid spacing={2} item>
+          <EventCard />
+        </Grid>
+      </Grid>
+    );
+  }
 }
-
