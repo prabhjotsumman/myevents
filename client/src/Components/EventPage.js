b01e64dbfceb4cdf227589e9d8fd62b3a9c1601c
prabhnith <prabhjot.nith@gmail.com>
2019-07-06 19:05:17 +0530

    EventPage: Speaker, description, venue cards added

--- client/src/Components/EventPage.js
+++ client/src/Components/EventPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AppBar from './Homepage/PrimarySearchAppBar';
-import { Grid, Typography, Button, Icon } from '@material-ui/core';
+import { Grid, Typography, Button, Icon, Box } from '@material-ui/core';
 import Paper from './Paper';
 
 export default class EventPage extends Component {
@@ -38,42 +38,64 @@ export default class EventPage extends Component {
                             <img src="https://picsum.photos/1100/400" alt="poster" style={{ width: '100%', height: 'auto' }} />
                             {
                                 (this.state.eventInfo) ? (
-                                    <Paper>
-                                        <Grid container justify="space-between" alignItems="flex-end">
-                                            <Grid item >
-                                                <Typography variant="h4" gutterBottom>{this.state.eventInfo[0].eventName}</Typography>
+                                    <>
+                                        <Paper>
+                                            <Grid container justify="space-between" alignItems="flex-end">
+                                                <Grid item >
+                                                    <Typography variant="h4" gutterBottom>{this.state.eventInfo[0].eventName}</Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Button size="large" variant="contained" color="primary">Book</Button>
+                                                </Grid>
                                             </Grid>
-                                            <Grid item>
-                                                <Button size="large" variant="contained" color="primary">Book</Button>
+                                            <Grid container spacing={1} >
+                                                <Grid item>
+                                                    <Icon color={"error"}>location_on</Icon>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography variant={"subtitle1"} gutterBottom>
+                                                        {this.state.eventInfo[0].eventVenue}
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Icon color={"primary"}>event</Icon>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography variant={"subtitle1"} gutterBottom>
+                                                        {this.state.eventInfo[0].eventStartDate}
+                                                    </Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Icon color={"primary"}>access_time</Icon>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Typography variant={"subtitle1"} gutterBottom>
+                                                        {this.state.eventInfo[0].eventStartTime}
+                                                    </Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </Paper>
+                                        <Box m={2}></Box>
+                                        <Grid container spacing={3}>
+                                            <Grid item xs={2} sm={2} md={2} >
+                                                <Paper>
+                                                    <Typography gutterBottom variant="subtitle2">About Speaker(s) </Typography>
+                                                    <Typography gutterBottom variant="body1">Speaker Name</Typography>
+                                                </Paper>
+                                            </Grid>
+                                            <Grid item xs={7} sm={7} md={7}>
+                                                <Paper>
+                                                    <Typography gutterBottom variant="subtitle2">Description </Typography>
+                                                    <Typography gutterBottom variant="body1">{this.state.eventInfo[0].eventDescription}</Typography>
+                                                </Paper>
+                                            </Grid>
+                                            <Grid item xs={3} sm={3} md={3}>
+                                                <Paper>
+                                                    <Typography gutterBottom variant="subtitle2">Venue </Typography>
+                                                </Paper>
                                             </Grid>
                                         </Grid>
-                                        <Grid container spacing={1} >
-                                            <Grid item>
-                                                <Icon color={"error"}>location_on</Icon>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant={"subtitle1"} gutterBottom>
-                                                    {this.state.eventInfo[0].eventVenue}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Icon color={"primary"}>event</Icon>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant={"subtitle1"} gutterBottom>
-                                                    {this.state.eventInfo[0].eventStartDate}
-                                                </Typography>
-                                            </Grid>
-                                            <Grid item>
-                                                <Icon color={"primary"}>access_time</Icon>
-                                            </Grid>
-                                            <Grid item>
-                                                <Typography variant={"subtitle1"} gutterBottom>
-                                                    {this.state.eventInfo[0].eventStartTime}
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </Paper>
+                                    </>
                                 ) : ""
                             }
                         </Grid>
