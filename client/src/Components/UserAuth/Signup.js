705e742214e6c19b6bb9323a6ef04b43bd078d83
prabhnith <prabhjot.nith@gmail.com>
2019-07-07 12:24:07 +0530

    MongoDB added locally,

--- client/src/Components/UserAuth/Signup.js
+++ client/src/Components/UserAuth/Signup.js
@@ -1,160 +1,47 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-// import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {Link as RLink} from 'react-router-dom';
-import Snackbar from '../Snackbar';
+import React, { Component } from 'react';
+import SignUpSide from './SignUpSide';
+import { Redirect } from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
-
-function handleSubmit(e){
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    var obj = {};
-    formData.forEach((value, key) => { obj[key] = value });
-    var json = JSON.stringify(obj);
-    console.log(obj);
-    fetch('http://localhost:3001/putUser',{
-        method :'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body:json
-    })
-    .then(res => res.json())
-    .then( json => {
-        console.log(json);
-        if(json.success){
-            //TODO save state and redirect to dashboard
+export default class SignIn extends Component {
+    constructor() {
+        super();
+        this.state = {
+            success: false,
+            status: null,
+            error: null,
+            loggedin: false
         }
-    })
-}
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        var obj = {};
+        formData.forEach((value, key) => { obj[key] = value });
+        var json = JSON.stringify(obj);
+        console.log(obj);
+        fetch('http://localhost:3001/putUser', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: json
+        })
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                if (json.success) {
+                    //TODO save state and redirect to dashboard
+                    this.setState({ success: json.success });
+                }
+            })
+    }
 
-export default function SignUp(){
-    const classes = useStyles();
-
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-        </Typography>
-                <form className={classes.form} noValidate onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                autoComplete="name"
-                                name="name"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="name"
-                                label="Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="organisationName"
-                                label="Organisation Name"
-                                name="organisationName"
-                                autoComplete="organisationName"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="mobile"
-                                label="Mobile"
-                                name="mobile"
-                                autoComplete="mobile"
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Register
-          </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <RLink to="/signin" variant="body2">
-                                Already have an account? Sign in
-              </RLink>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
-    );
+    render() {
+        return (
+            (this.state.success) ?
+                <Redirect to="/Dashboard" /> :
+                <SignUpSide onRegister={this.handleSubmit} />
+        )
+    }
 }
